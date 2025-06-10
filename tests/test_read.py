5338377721625aff5f99636437bc9ec23b596e33
Flavien LE BLOND <flavien.leblond@tenacy.io>
2025-06-10 21:37:17 +0200

    fix(comment): Remove useless comment

--- tests/test_read.py
+++ tests/test_read.py
@@ -1,23 +1,5 @@
 from domain.bet import BettingElement, BettingList, Strategy
 from read import start_read_mode
-# TO FINISH TESTING THIS
-# def start_read_mode(filename: str = "json/default_json.json"):
-#     data = read_json_file(file_name="json/default_json.json")
-#     if not data:
-#         return None
-
-#     betting_list: Optional[BettingList] = json_to_betting_list(
-#         content_to_load=data, strategy_name=Strategy.RISKY
-#     )
-#     if not betting_list:
-#         return None
-
-#     table = generate_table_from_betting_list(
-#         betting_list=betting_list, table_title="Bet history"
-#     )
-
-#     console.print(table)
-#     return betting_list
 
 EXPECTED_ALL_DICT = [
     {
